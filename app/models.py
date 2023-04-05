12b5d4b000ab8a586eca98de865a4bd67d22d3d0
Ersin Nurtin <ersin.nurtin@outlook.com>
2023-04-05 17:03:20 +0300

    added registration functionality

--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
-from app import db
+from flask_login import UserMixin
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+	
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	def avatar(self, size):
+		pass
 	
 	def __repr__(self):
 		return f"<User {self.username}>"
@@ -18,6 +30,11 @@ class Post(db.Model):
 	body = db.Column(db.String(140))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+	
 	def __repr__(self):
 		return f"<Post {self.body}>"
+
+
+@login.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
